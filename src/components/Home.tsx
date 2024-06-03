df37b883912eece551b7446532f31452137dcb98
Somrita Banerjee <somritabanerjee126@gmail.com>
2024-06-03 19:29:16 +0530

    tailwind changes in Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,12 +8,20 @@ function Home() {
     }
 
     return (
-    <div className="App-main">
-        <div className="center-container">
-            <button className="home-buttons" type="submit" onClick={() => handleClick('/play')}>PLAY</button>
+    <div className="bg-purple-300 p-20">
+        <div className="flex justify-center items-center">
+            <button
+                className="h-20 m-5 rounded-2xl text-5xl bg-[#B61BA1] w-96 text-white shadow-md hover:shadow-2xl hover:cursor-pointer home-buttons"
+                type="submit"
+                onClick={() => handleClick('/play')}
+                >PLAY</button>
         </div>
-        <div className="center-container">
-            <button className="home-buttons" type="submit" onClick={() => handleClick('/solve')}>SOLVE</button>
+        <div className="flex justify-center items-center">
+            <button
+                className="h-20 m-5 rounded-2xl text-5xl bg-[#B61BA1] w-96 text-white shadow-md hover:shadow-2xl hover:cursor-pointer home-buttons"
+                type="submit"
+                onClick={() => handleClick('/solve')}
+            >SOLVE</button>
         </div>
         
     </div>
